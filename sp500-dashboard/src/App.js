d2b4e14545417bf3bdd340d2c855483581e28bc7
Spoorthy K R <spoorthykr1801@gmail.com>
2025-08-05 18:45:01 +0530

    updated app.js

--- sp500-dashboard/src/App.js
+++ sp500-dashboard/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [selected, setSelected] = useState('');
   const [plots, setPlots] = useState([]);
   const FLASK_BACKEND_URL = 'https://financial-data-analysis.onrender.com';
+  const FLASK_BACKEND_PLOT_URL = 'https://financial-data-analysis.onrender.com/static/plots/';
 
 
   useEffect(() => {
@@ -30,7 +31,7 @@ function App() {
         {plots.map(plot => (
           <div key={plot.title}>
             <h3>{plot.title}</h3>
-            <img src={plot.url} alt={plot.title} style={{maxWidth: '600px'}} />
+            <img src={`${FLASK_BACKEND_PLOT_URL}${plot.url}`} alt={plot.title} style={{maxWidth: '600px'}} />
             <p>{plot.explanation}</p>
           </div>
         ))}
